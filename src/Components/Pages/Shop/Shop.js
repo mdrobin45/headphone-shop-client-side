bb80ed716ca4ea9cf91ed344b614fd4525bba5de
Md Robin Rana <robin4500bd@gmail.com>
2021-11-13 10:36:08 +0600

    shop page created

--- src/Components/Pages/Shop/Shop.js
+++ src/Components/Pages/Shop/Shop.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import useAPI from '../../../Hooks/useAPI';
+import SectionHeader from '../../SectionHeader/SectionHeader';
+import SingleHeadPhone from '../Home/HomeShop/SingleHeadPhone/SingleHeadPhone';
 
-const Shop = () => {
+const Shop = () =>
+{
+    const { headPhones } = useAPI().fetchData;
     return (
-        <div>
-            <h2>Shop</h2>
+        <div className='container py-20'>
+            <div className='text-center'>
+                <SectionHeader mainTitle='Choose Colors' subTitle='SHOP HEADPHONES'/>
+            </div>
+            <div className='grid pt-6 grid-cols-3 gap-4'>
+                {
+                   headPhones?.slice(0,6).map(headPhone=><SingleHeadPhone key={headPhone._id} headPhone={headPhone}/>)
+                }
+            </div>
         </div>
     );
 };
