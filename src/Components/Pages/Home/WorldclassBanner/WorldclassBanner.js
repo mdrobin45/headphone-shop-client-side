bad806a73542ba56b3eedb1ce2db0cc663fcfa28
Md Robin Rana <robin4500bd@gmail.com>
2021-11-13 20:18:41 +0600

    y

--- src/Components/Pages/Home/WorldclassBanner/WorldclassBanner.js
+++ src/Components/Pages/Home/WorldclassBanner/WorldclassBanner.js
@@ -7,8 +7,8 @@ const WorldclassBanner = () => {
         <div className='worldClassBanner'>
             <div className='container'>
                  <div className='md:w-2/4'>
-                    <h4 className='text-xl tracking-widest font-primary text-white'>WORLDCLASS WIRELESS HEADPHONES</h4>
-                    <h2 className='font-primary text-6xl font-bold text-white'>Experience the next level of silence.</h2>
+                    <h4 className='md:text-xl tracking-widest font-primary text-white'>WORLDCLASS WIRELESS HEADPHONES</h4>
+                    <h2 className='font-primary md:font-bold md:text-6xl text-3xl text-white'>Experience the next level of silence.</h2>
                     <Button url='/shop' btnText='Discover now'/>
                 </div>
                 <div className='md:w-2/4 block'></div>
